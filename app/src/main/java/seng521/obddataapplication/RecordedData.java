25a9945bbcf2dcab9fd70c3bc2b3c41336425294
Zoey Lan <zozoee27@gmail.com>
2016-11-21 16:48:23 -0700

    Added Trip Object, and _id for RecordedData

--- app/src/main/java/seng521/obddataapplication/RecordedData.java
+++ app/src/main/java/seng521/obddataapplication/RecordedData.java
@@ -8,16 +8,25 @@ import java.util.Date;
 
 public class RecordedData {
 
-    private Date mTimestamp;
-    private double mLatitude; // y-coordinate
-    private double mLongitude; // x-coordinate
-    private double mSpeed;
+    private Date timestamp;
+    private double latitude; // y-coordinate
+    private double longitude; // x-coordinate
+    private double speed;
+    private String _id;
 
     public RecordedData(Date timestamp, double latitude, double longitude, double speed) {
-        mTimestamp = timestamp;
-        mLatitude = latitude;
-        mLongitude = longitude;
-        mSpeed = speed;
+        this.timestamp = timestamp;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.speed = speed;
     }
-    
+
+    public RecordedData(Date timestamp, double latitude, double longitude, double speed, String _id) {
+        this.timestamp = timestamp;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.speed = speed;
+        this._id = _id;
+    }
+
 }
---
+++ app/src/main/java/seng521/obddataapplication/Trip.java
@@ -0,0 +1,29 @@
+package seng521.obddataapplication;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by ZoeyLan on 2016-11-21.
+ */
+
+public class Trip {
+
+    private Date startTime;
+    private Date endTime;
+    private String vehicleId;
+    private boolean inProgress;
+    private String _id;
+    private List<RecordedData> recordedData;
+
+    public Trip(Date startTime, Date endTime, String vehicleId, boolean inProgress)
+    {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.vehicleId = vehicleId;
+        this.inProgress = inProgress;
+    }
+
+
+
+}
