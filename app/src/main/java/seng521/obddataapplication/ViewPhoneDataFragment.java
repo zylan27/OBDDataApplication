5c3944b5ce62052d76c5d5b9eeb7e6bc4b003636
Zoey Lan <zozoee27@gmail.com>
2016-11-28 16:25:15 -0700

    Added tabbed view for trip data

---
+++ app/src/main/java/seng521/obddataapplication/ViewPhoneDataFragment.java
@@ -0,0 +1,90 @@
+package seng521.obddataapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
+ * interface.
+ */
+public class ViewPhoneDataFragment extends Fragment {
+
+    private OnListFragmentInteractionListener mListener;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public ViewPhoneDataFragment() {
+    }
+
+    public static ViewPhoneDataFragment newInstance() {
+        ViewPhoneDataFragment fragment = new ViewPhoneDataFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_phone_data, container, false);
+
+        // Display list;
+
+
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnListFragmentInteractionListener) {
+            mListener = (OnListFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnListFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onPhoneDataFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/seng521/obddataapplication/ViewTripsFragment.java
+++ app/src/main/java/seng521/obddataapplication/ViewTripsFragment.java
@@ -126,7 +126,7 @@ public class ViewTripsFragment extends Fragment {
                 // use response.code, response.headers, etc.
                 Log.d("myTag", "Get Trips: Success");
 
-                List<Trip> trips = response.body();
+                final List<Trip> trips = response.body();
 
                 Log.d("myTag", "Printing Trips");
                 Log.d("myTag", trips.toString());
@@ -143,13 +143,12 @@ public class ViewTripsFragment extends Fragment {
                     {
                             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
                             {
-
-                                TextView textView = (TextView) view.findViewById(R.id.tripNumberTextId);
-                                String selectedValue = textView.getText().toString();
-                                Log.d("myTag", "printing selected item text");
-                                Log.d("myTag", selectedValue);
+                                Trip trip = trips.get(position);
                                 Intent intent = new Intent(getActivity().getApplicationContext(), ViewTripDataActivity.class);
-                                intent.putExtra("selectedValue ", selectedValue);
+                                intent.putExtra("tripId", trip.getId());
+                                intent.putExtra("startTime", trip.getStartTime());
+                                intent.putExtra("endTime", trip.getEndTime());
+
                                 startActivity(intent);
                             }
                     });
